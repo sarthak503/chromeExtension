cdb0b3b8278672db6e43aad89fe948d997763f96
sarthak503 <sarthak.kumar503@gmail.com>
2022-01-29 23:21:30 +0530

    added manifest file and made it functional

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 let myleads = [];
 const inpbtn = document.getElementById("input-btn");
+const delbtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-inpbtn.addEventListener("click", function () {
-  myleads.push(inputEl.value);
-  inputEl.value = "";
-  renderLeads();
+const myleadsinLocal = JSON.parse(localStorage.getItem("myleads"));
+
+const tabbtn = document.getElementById("tab-btn");
+
+if (myleadsinLocal) {
+  myleads = myleadsinLocal;
+  render(myleads);
+}
+//const tabs = [{ url: "linkedin.com/home/?originalSubdomain=in" }];
+
+tabbtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs);
+    myleads.push(tabs[0].url);
+    localStorage.setItem("myleads", JSON.stringify(myleads));
+    render(myleads);
+  });
 });
-function renderLeads() {
+
+function render(leads) {
   let listItems = "";
-  for (let a = 0; a < myleads.length; a++) {
+  for (let a = 0; a < leads.length; a++) {
     // listItems +=
     //   "<li><a target='_blank' href='" +
     //   myleads[a] +
@@ -19,8 +34,8 @@ function renderLeads() {
     //   "</li>";
     listItems += `
     <li>
-     <a target='_blank' href='${myleads[a]}'> 
-     ${myleads[a]}
+     <a target='_blank' href='${leads[a]}'> 
+     ${leads[a]}
      </a>
     </li>
     `;
@@ -30,3 +45,15 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItems;
 }
+delbtn.addEventListener("click", function () {
+  localStorage.clear();
+  myleads = [];
+  render(myleads);
+});
+
+inpbtn.addEventListener("click", function () {
+  myleads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myleads", JSON.stringify(myleads));
+  render(myleads);
+});
